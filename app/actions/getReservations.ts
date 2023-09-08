f0d4505e445eee43f28c11b45a332d045384f7c2
harsh661 <plasmaharsh@gmail.com>
2023-09-08 15:55:52 +0530

    feat: get reservation action created

---
+++ app/actions/getReservations.ts
@@ -0,0 +1,51 @@
+import prisma from "../libs/prismadb"
+
+interface reservationParams {
+  listingId?: string
+  userId?: string
+  authorId?: string
+}
+
+export default async function getReservation(params: reservationParams) {
+  try {
+    const { listingId, userId, authorId } = params
+    const query: any = {}
+
+    if (listingId) {
+      query.listingId = listingId
+    }
+
+    if (userId) {
+      query.userId = userId
+    }
+
+    if (authorId) {
+      query.authorId = authorId
+    }
+
+    const reservations = await prisma.reservation.findMany({
+      where: query,
+      include: {
+        listing: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    })
+
+    const safeReservations = reservations.map((reservation) => ({
+      ...reservation,
+      createdAt: reservation.createdAt.toISOString(),
+      startDate: reservation.startDate.toISOString(),
+      endDate: reservation.endDate.toISOString(),
+      listing: {
+        ...reservation.listing,
+        createdAt: reservation.listing.createdAt.toISOString(),
+      },
+    }))
+
+    return safeReservations
+  } catch (error: any) {
+    throw new Error(error)
+  }
+}
